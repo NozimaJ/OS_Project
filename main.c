55bce55cb1be6b34415b9b6eacdee990f3114383
ShakhzodaYunusova <shakhzoda1996@mail.ru>
2018-12-17 01:19:40 +0500

    new

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <gtk/gtk.h>   
+
+//creating objects
+GtkBuilder      *builder; 
+GtkWidget       *main_page;
+GtkWidget       *register_page;
+GtkWidget       *client_page;
+
+int main(int argc, char *argv[])
+{
+
+  gtk_init(&argc, &argv);
+
+  //adding glade file to our builder
+  builder = gtk_builder_new();
+  gtk_builder_add_from_file (builder, "gui.glade", NULL);
+
+  //assigning window widget with already defined glade file id window_main
+  main_page = GTK_WIDGET(gtk_builder_get_object(builder, "main_page"));
+  register_page = GTK_WIDGET(gtk_builder_get_object(builder, "register_page"));
+  client_page = GTK_WIDGET(gtk_builder_get_object(builder, "client_page"));
+  gtk_builder_connect_signals(builder, NULL);
+
+  g_object_unref(builder);
+
+  gtk_widget_show(main_page);                
+  gtk_main();
+
+  return 0;
+}
+
+// called when window is closed
+void on_window_main_destroy()
+{
+  gtk_main_quit();
+}
+
+void on_register_clicked()
+{
+    gtk_widget_hide(main_page);
+    gtk_widget_show(register_page);  
+}
+void on_back_clicked()
+{
+	gtk_widget_hide(register_page);
+	gtk_widget_show(main_page);
+}
+void on_user_clicked()
+{
+	gtk_widget_hide(main_page);
+	gtk_widget_show(client_page);
+}
+void on_back2_clicked()
+{
+	gtk_widget_hide(client_page);
+	gtk_widget_show(main_page);
+}
+
+
